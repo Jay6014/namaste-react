d4b170a2f3e86001d554361af63fddf37d1d4ce7
unknown <kodamanchilijayasree@gmail.com>
2025-10-24 15:04:03 +0530

    dist folder

---
+++ dist/Grocery.ef64ae24.js
@@ -0,0 +1,2 @@
+var e=globalThis.parcelRequirea907;(0,e.register)("0ceaH",function(r,o){Object.defineProperty(r.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(r.exports,"default",{get:()=>t,set:void 0,enumerable:!0,configurable:!0});var a=e("2Qgwn");e("3qC2B");var t=()=>(0,a.jsx)("h1",{children:"Our Grocery online store"})});
+//# sourceMappingURL=Grocery.ef64ae24.js.map
